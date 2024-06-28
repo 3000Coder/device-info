5ec35411a80eada193aef230cfa0768d1dd006a7
3000Coder <173622885+3000Coder@users.noreply.github.com>
2024-06-28 18:12:57 +0200

    Add more values and code cleanup

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template("main.html", ip=request.remote_addr)
